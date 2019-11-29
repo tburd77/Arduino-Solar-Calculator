f7c170ee14b22076f305194dc9b789cd10b593e9
tburd77 <46765026+tburd77@users.noreply.github.com>
2019-11-29 10:07:28 -0800

    JulianDateCalc.h

---
+++ JulianDateCalc.h
@@ -0,0 +1,21 @@
+//GPS
+#include <TinyGPS++.h>
+#include <SoftwareSerial.h>
+//RTC
+#include <TimeLib.h>
+#include <Time.h>
+#include <DS3232RTC.h>
+#define I2C_Clock_ADDR          0x68
+#include "Wire.h"
+//OLED
+#include "U8glib.h"
+#define I2C_OLED_ADDR          0x3C
+
+
+#define myLatitude    48.277              
+#define myLongitude  -116.4337
+
+#define DEG_TO_RAD    0.017453292519943295769236907684886
+
+
+
